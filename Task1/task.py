ec17f5057e6b0b4668776f38c87a47dd53222722
M.Muzafarov <m.muzafarov@gmail.com>
2013-03-16 06:34:49 +0100

    Test

--- Task1/task.py
+++ Task1/task.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python
-import os
 import sys
-import Tkinter
+from Tkinter import *
 from PIL import Image
 
 
 def button_exit():
     sys.exit(1)
 
-root = Tkinter.Tk()
-root.bind("<Button>", button_exit)
-root.geometry('+%d+%d' % (400, 400))
+
+root = Tk()
+root.title("Test")
+canv = Canvas(root,
+              width=800, height=500,
+              bg="lightgray")
+canv.pack()
 try:
     image = Image.open("../Test_Images/image_House256rgb.png")
-    tkpi = Tkinter.PhotoImage(image)
-    label_image = Tkinter.Label(root, image=tkpi)
+    tkpi = PhotoImage(image)
+    label_image = Label(root, image=tkpi)
     label_image.place(x=0, y=0, width=image.size[0], height=image.size[1])
-    root.title("Test")
-    root.mainloop()
 except Exception as e:
     pass
+finally:
+    root.mainloop()
